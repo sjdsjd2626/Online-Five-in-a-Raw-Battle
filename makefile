496aa3adc6af07a40a2dcd5a87e3e874c74d8d9f
巨新豪 <juxinhao2022@163.com>
2025-03-01 17:00:00 +0800

    websocket简单服务器搭建

---
+++ makefile
@@ -0,0 +1,2 @@
+wss:wsserver.cpp
+	g++ -o $@ $^ -std=c++11 -lpthread -lboost_system
---
+++ wsserver.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<functional>
+#include<websocketpp/server.hpp>
+#include<websocketpp/config/asio_no_tls.hpp>
+
+typedef websocketpp::server<websocketpp::config::asio>wsserver_t;
+void wshttp_callback(wsserver_t*srv,websocketpp::connection_hdl hd1)
+{
+    wsserver_t::connection_ptr conn=srv->get_con_from_hdl(hd1);
+    std::cout<<"body "<<conn->get_request_body()<<std::endl;
+    websocketpp::http::parser::request req=conn->get_request();
+    std::cout<<"method "<<req.get_method()<<std::endl;
+    std::cout<<"uri "<<req.get_uri()<<std::endl;
+
+    std::string body="<html><body><h1>Hello World</h1></body></html>";
+    conn->set_body(body);
+    conn->append_header("Content-Type","text/html");
+    conn->set_status(websocketpp::http::status_code::ok);
+}
+void wsopen_callback(wsserver_t*srv,websocketpp::connection_hdl hd1)
+{
+    std::cout<<"websocket握手成功!!"<<std::endl;
+}
+void wsclose_callback(wsserver_t*srv,websocketpp::connection_hdl hd1)
+{
+    std::cout<<"websocket连接断开!!"<<std::endl;
+}
+void wsmsg_callback(wsserver_t*srv,websocketpp::connection_hdl hd1,wsserver_t::message_ptr msg)
+{
+    wsserver_t::connection_ptr conn=srv->get_con_from_hdl(hd1);
+    std::cout<<"wsmsg: "<<msg->get_payload()<<std::endl;
+    std::string rsp ="client say: "+msg->get_payload();
+    conn->send(rsp,websocketpp::frame::opcode::text);
+}
+int main()
+{
+    //1.实例化server对象
+    wsserver_t wssrv;
+    //2.设置日志等级
+    wssrv.set_access_channels(websocketpp::log::alevel::none);
+    //3.初始化asio调度器
+    wssrv.init_asio();
+    wssrv.set_reuse_addr(true);
+    //4.设置回调函数
+    wssrv.set_http_handler(std::bind(wshttp_callback,&wssrv,std::placeholders::_1));//从左到右是形参的顺序，1，2是实参
+    wssrv.set_open_handler(std::bind(wsopen_callback,&wssrv,std::placeholders::_1));
+    wssrv.set_close_handler(std::bind(wsclose_callback,&wssrv,std::placeholders::_1));
+    wssrv.set_message_handler(std::bind(wsmsg_callback,&wssrv,std::placeholders::_1,std::placeholders::_2));
+    //5.设置监听端口
+    wssrv.listen(5566);
+    //6.开始获取新连接
+    wssrv.start_accept();
+    //7.启动服务器
+    wssrv.run();
+    return 0;
+}
